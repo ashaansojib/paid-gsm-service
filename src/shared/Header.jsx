1e3db2b85efc926304d7dac1a7be9668205bf8da
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-02-04 02:24:22 +0600

    header structure done

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -11,6 +11,22 @@ const Header = () => {
                     <p>Phone: (+880) 1892932859</p>
                 </div>
             </div>
+            {/* main header area */}
+            <div className='my-container h-[70px] grid grid-cols-4 gap-3 justify-between items-center'>
+                {/* logo area */}
+                <div className="">
+                    <h2 className='text-xl font-semibold'>O-GSMService</h2>
+                </div>
+                {/* search box */}
+                <div className='col-span-2'>
+                    <input className='inline-block w-full p-3 border' type="text" placeholder='Search File' name="" id="" />
+                </div>
+                {/* user profile and carts area */}
+                <div className='flex items-center justify-end gap-2'>
+                    <p>Balance: 0 BDT</p>
+                    <button className='px-1 py-2 bg-slate-100 rounded-sm hover:bg-blue-600 hover:text-white transition'>Dashboard</button>
+                </div>
+            </div>
         </div>
     );
 };
