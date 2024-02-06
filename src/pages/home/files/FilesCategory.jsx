56879230f2e0c93c5f048a38af4cd7732bc4bd16
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-02-06 21:10:55 +0600

    add files link

--- src/pages/home/files/FilesCategory.jsx
+++ src/pages/home/files/FilesCategory.jsx
@@ -6,12 +6,12 @@ const FilesCategory = ({ singleFile }) => {
   return (
     <div className="flex gap-2 bg-white my-2">
       <div className="w-[90px]">
-        <Link to="/file">
+        <Link to="/category/ADB%20Driver">
           <img src="/folder.png" alt="" />
         </Link>
       </div>
       <div>
-        <Link to="/file">
+        <Link to="/category/ADB%20Driver">
           <h2 className="text-xl font-semibold">{name} is the Name</h2>
         </Link>
         <p className="italic text-slate-600 text-sm">
