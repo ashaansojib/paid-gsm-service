54672d8d2ce0f55c2e1740a8ef578577b9984af8
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-02-18 18:51:13 +0600

    fixed admin aside for mobile view

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AdminHeader from '../pages/dashboard/AdminHeader';
 import AdminSidebar from '../pages/dashboard/AdminSidebar';
 import AdminFooter from '../pages/dashboard/AdminFooter';
 import { Outlet } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa6';
 
 const Dashboard = () => {
+    const [showAside, setShowAside] = useState(false);
     return (
         <div>
             <AdminHeader />
-            <div className='my-container grid grid-cols-4 justify-between '>
-                <AdminSidebar />
-                <div className='col-span-3 border p-2'>
+            <FaBars onClick={()=> setShowAside(!showAside)} className='md:hidden block text-3xl absolute top-2 right-2 cursor-pointer' />
+            <div className='my-container grid md:grid-cols-4 grid-cols-1 justify-between '>
+                <div className={`${showAside ? 'block' : 'hidden'}`}>
+                    <AdminSidebar />
+                </div>
+                <div className='col-span-3 border p-4 m-2 md:m-0'>
                     <Outlet />
                 </div>
             </div>
--- src/pages/dashboard/pages/AddService.jsx
+++ src/pages/dashboard/pages/AddService.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const AddService = () => {
     return (
-        <div>
-            here you can add servcices..
+        <div className='grid md:grid-cols-2 grid-cols-1 justify-between gap-3 items-center'>
+            <input type="text" placeholder="Brand" className="input input-bordered w-full" />
+            <input type="text" placeholder="Model" className="input input-bordered w-full" />
+            <input type="text" placeholder="Codename" className="input input-bordered w-full" />
+            <input type="text" placeholder="Services" className="input input-bordered w-full" />
+            <button className="btn btn-wide">PostNow</button>
         </div>
     );
 };
