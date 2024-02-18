23b3520954cf2913fbe9f542979f1986aba0faaa
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-02-18 18:08:54 +0600

    dynamic menu ready

--- src/shared/MenuBar.jsx
+++ src/shared/MenuBar.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { FaBars } from 'react-icons/fa6'
+import { useState } from "react";
 const MenuBar = () => {
+  const [showMenu, setShowMenu] = useState(true);
   return (
     <div className="bg-[#211C6A]">
-      <div className="my-container">
-        <ul className="flex flex-col md:flex-row md:items-center">
+      <div className="my-container relative">
+        <FaBars onClick={()=> setShowMenu(!showMenu)} className="text-3xl cursor-pointer md:hidden block absolute right-2 -top-8" />
+        <ul className={`flex md:flex-row flex-col md:items-center ${showMenu ? 'block' : 'hidden'}`}>
           <Link className="nav-items" to="/">
             Home
           </Link>
