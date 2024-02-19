83a75ba9696f184b9fb58ea7f4c3034e555b2b9a
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-02-19 10:24:39 +0600

    change top nav

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,51 +1,27 @@
-import React from 'react';
-import HeadingMarquee from './HeadingMarquee';
-import { Link, useLocation } from 'react-router-dom';
-import TopNav from './TopNav';
-import MenuBar from './MenuBar';
-import { FaCartArrowDown, FaEnvelope, FaPhone } from 'react-icons/fa6'
+import React from "react";
+import HeadingMarquee from "./HeadingMarquee";
+import { Link, useLocation } from "react-router-dom";
+import TopNav from "./TopNav";
+import MenuBar from "./MenuBar";
+import { FaCartArrowDown, FaEnvelope, FaPhone } from "react-icons/fa6";
 const Header = () => {
-    const location = useLocation();
-    return (
-        <div>
-            {/* top header content */}
-            <div className='my-container border-b md:flex justify-between py-3 px-2 hidden'>
-                <TopNav />
-                <div className='flex items-center gap-2'>
-                    <FaEnvelope />
-                    <Link className='text-hover text-sm' to="mailto:ashaduzzamansojib67@gmail.com">ogsmservice@bd</Link>|
-                    <FaPhone />
-                    <p className='text-sm'>+880-1892932859</p>
-                </div>
-            </div>
-            {/* main header area */}
-            <div className='my-container md:h-[155px] grid md:grid-cols-4 grid-cols-1 px-2 gap-3 md:justify-between justify-center items-center'>
-                {/* logo area */}
-                <div className="">
-                    <img className='h-[132px] w-[200px]' src="/logo.png" alt="" />
-                    {/* <h2 className='text-xl font-semibold'><Link to="/">O-GSMService</Link></h2> */}
-                </div>
-                {/* search box */}
-                <div className='col-span-2'>
-                    <input className='inline-block w-full p-3 border rounded-lg border-[#211C6A]' type="text" placeholder='Search File' name="" id="" />
-                </div>
-                {/* user profile and carts area */}
-                <div className='flex items-center md:justify-end gap-2'>
-                    <FaCartArrowDown className='text-3xl' />
-                    <Link to="/admin">
-                        <span>Your Carts: </span>
-                        <p className='text-xl font-semibold'>BDT : 0.0000</p>
-                    </Link>
-                </div>
-            </div>
-            {/* menu bar in header */}
-            <MenuBar />
-            {/* ads or updating news area */}
-            {
-                location.pathname === '/' ? <HeadingMarquee /> : ''
-            }
+  const location = useLocation();
+  return (
+    <div>
+      {/* top header content */}
+      <div className="bg-[#24272B]">
+        <div className="my-container md:flex justify-between items-center py-4 px-2 hidden">
+          <h2 className="text-white text-2xl italic">O-GsmService</h2>
+          <TopNav />
         </div>
-    );
+      </div>
+      
+      {/* menu bar in header */}
+      <MenuBar />
+      {/* ads or updating news area */}
+      {location.pathname === "/" ? <HeadingMarquee /> : ""}
+    </div>
+  );
 };
 
 export default Header;
--- src/shared/TopNav.jsx
+++ src/shared/TopNav.jsx
@@ -3,18 +3,17 @@ import { Link } from "react-router-dom";
 
 const TopNav = () => {
   return (
-    <div>
-      <ul className="flex gap-3 items-center">
-        <Link className="text-hover font-semibold text-sm" to="about-us">
-          About Us
-        </Link>
-        <Link className="text-hover font-semibold text-sm" to="#">
-          Services
-        </Link>
-        <Link className="text-hover font-semibold text-sm" to="#">
-          Imei Check
-        </Link>
-      </ul>
+    <div className="space-x-4 text-gray-300">
+      <Link className="text-sm hover:text-white" to="about-us">
+        About Us
+      </Link>
+      <Link className="text-sm hover:text-white" to="#">
+        Services
+      </Link>
+      <Link className="text-sm hover:text-white" to="#">
+        Imei Check
+      </Link>
+      <button className="rounded-md px-3 py-1 hover:text-white bg-[#36393C] hover:bg-[#21C263]"><Link to="/admin">Singup</Link></button>
     </div>
   );
 };
