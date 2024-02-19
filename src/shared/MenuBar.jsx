50803dcaf4f05f8125a60c01741c118181ef4d7b
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-02-19 19:00:13 +0600

    complete menu bar

--- src/shared/MenuBar.jsx
+++ src/shared/MenuBar.jsx
@@ -1,38 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { FaBars } from 'react-icons/fa6'
 import { useState } from "react";
 const MenuBar = () => {
   const [showMenu, setShowMenu] = useState(true);
   return (
-    <div className="bg-[#211C6A]">
+    <div className="bg-[#2B2E32]">
       <div className="my-container relative">
-        <FaBars onClick={()=> setShowMenu(!showMenu)} className="text-3xl cursor-pointer md:hidden block absolute right-2 -top-8" />
-        <ul className={`flex md:flex-row flex-col md:items-center ${showMenu ? 'block' : 'hidden'}`}>
-          <Link className="nav-items" to="/">
+        <FaBars onClick={() => setShowMenu(!showMenu)} className="text-3xl cursor-pointer p-1 absolute right-2 top-2 md:hidden block bg-white" />
+        <ul className={`p-2 md:p-0 flex md:flex-row flex-col md:gap-6 md:items-center ${showMenu ? 'block' : 'hidden'}`}>
+          <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')} to="/">
             Home
-          </Link>
-          <Link className="nav-items" to="/price">
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')} to="/price">
             Price & Packages
-          </Link>
-          <Link className="nav-items" to="/category/Sony%20USB%20Driver">
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')} to="/category/Sony%20USB%20Driver">
             Update Tools
-          </Link>
-          <Link className="nav-items" to="/unlocking-services">
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')} to="/unlocking-services">
             Services
-          </Link>
-          <Link className="nav-items" to="/blogs">
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')} to="/blogs">
             Blogs
-          </Link>
-          <Link className="nav-items" to="/reseller">
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')} to="/reseller">
             Our Agents
-          </Link>
-          <Link className="nav-items" to="/user-request">
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')} to="/user-request">
             Request File
-          </Link>
-          <Link className="nav-items" to="/contact">
+          </NavLink>
+          <NavLink className={({ isActive }) => (isActive ? 'active' : 'default')} to="/contact">
             Contact
-          </Link>
+          </NavLink>
         </ul>
       </div>
     </div>
