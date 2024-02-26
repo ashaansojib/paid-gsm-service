ebe13fe08361ae577564b7cc82d78fa9b4153614
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-02-26 19:41:17 +0600

    add server link

--- src/redux/features/baseApi.js
+++ src/redux/features/baseApi.js
@@ -1,89 +1,95 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:9988" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://o-gsmservice-server.vercel.app/" }),
   endpoints: (builder) => ({
-    // posts for blog
-    getBlogs: builder.query({
-      query: () => "/blogs",
-    }),
-    addBlogs: builder.mutation({
-      query: (data) => ({
-        url: "/add-blog",
-        method: "POST",
-        body: data,
-      }),
-    }),
-    // single file and section
-    getAllFiles: builder.query({
-      query: () => "/all-files",
-    }),
-    addFile: builder.mutation({
-      query: (data) => ({
-        url: "/add-file",
-        method: "POST",
-        body: data,
-      }),
-    }),
-    removeFile: builder.mutation({
-      query: (id) => ({
-        url: `/remove-post/${id}`,
-        method: 'DELETE',
-      }),
-    }),
-    getUniquePost: builder.query({
-      query: () => ({
-        url: '/unique-posts',
-      }),
-    }),
+
     // tools and driver section--------------------------------
     getTools: builder.query({
-      query: () => ({
-        url: "/all-tools",
-      }),
+      query: () => "all-tools"
     }),
     addTool: builder.mutation({
       query: (data) => ({
-        url: "/add-tool",
+        url: "add-tool",
         method: "POST",
         body: data,
       }),
     }),
     getToolAndDriver: builder.query({
-      query: () => "/unique-brands",
+      query: () => "unique-tools",
     }),
     removeTool: builder.mutation({
       query: (id) => ({
-        url: `/remove-tool/${id}`,
+        url: `remove-tool/${id}`,
         method: "DELETE",
       }),
     }),
-    // service section------------------------------------
+
+    // flashfile section and section------------------------------------
+    getAllFiles: builder.query({
+      query: () => "all-files",
+    }),
+    addFile: builder.mutation({
+      query: (data) => ({
+        url: "add-file",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    getUniquePost: builder.query({
+      query: () => "unique-posts",
+    }),
+    removeFile: builder.mutation({
+      query: (id) => ({
+        url: `remove-post/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+
+    // online service section------------------------------------
     getServices: builder.query({
-      query: () => "/o-services",
+      query: () => "o-services",
     }),
     addService: builder.mutation({
       query: (data) => ({
-        url: "/add-service",
+        url: "add-service",
         method: "POST",
         body: data,
       }),
     }),
+
+    // posts for blog
+    // getBlogs: builder.query({
+    //   query: () => "blogs",
+    // }),
+    // addBlogs: builder.mutation({
+    //   query: (data) => ({
+    //     url: "add-blog",
+    //     method: "POST",
+    //     body: data,
+    //   }),
+    // }),
+
+
     // agents apis
-    getAgent: builder.query({
-      query: () => "/agents",
-    }),
-    addAgent: builder.mutation({
-      query: (data) => ({
-        url: "/add-agent",
-        method: "POST",
-        body: data,
-      }),
-    }),
+    // getAgent: builder.query({
+    //   query: () => "agents",
+    // }),
+    // addAgent: builder.mutation({
+    //   query: (data) => ({
+    //     url: "add-agent",
+    //     method: "POST",
+    //     body: data,
+    //   }),
+    // }),
   }),
 });
 
 export const {
+  useGetToolsQuery,
+  useGetToolAndDriverQuery,
+  useAddToolMutation,
+  useRemoveToolMutation,
   useGetAllFilesQuery,
   useGetUniquePostQuery,
   useAddFileMutation,
@@ -93,9 +99,5 @@ export const {
   useGetBlogsQuery,
   useAddBlogsMutation,
   useGetAgentQuery,
-  useAddAgentMutation,
-  useGetToolsQuery,
-  useGetToolAndDriverQuery,
-  useAddToolMutation,
-  useRemoveToolMutation,
+  useAddAgentMutation
 } = baseApi;
